2a67498dbd6e7f71dd6fd1e2719e0a3f062c80cc
Fabricio Oliveira <fabricio.v.o@hotmail.com>
2022-04-19 21:48:16 -0300

    Player movement

--- JavaScript/Entities/Player.js
+++ JavaScript/Entities/Player.js
@@ -2,44 +2,73 @@
 var LEFT = 1;
 var RIGHT = 2;
 
-function Player(context, image, keyboard,fireballImage) {
-	this.context = context;
-	this.image = image;
-	this.keyboard = keyboard;
-	this.x = 96;
-	this.y = 61;
-	this.w = 8;
-	this.h = 12;
-	this.speedX = 1;
-	this.dir = RIGHT;
-	this.fireball = new Array();
-	this.fireballImage = fireballImage;
-
+function Player(context, ImageSrc, keyboard, project) {
+  this.context = context;
+  this.image = new Image();
+  this.image.src = ImageSrc;
+  this.keyboard = new Keyboard(document);
+  this.x = 96;
+  this.y = 61;
+  this.w = 8;
+  this.h = 12;
+  this.speedX = 1;
+  this.dir = RIGHT;
+  this.fireball = new Array();
+  this.project = project;
+  this.input();
 }
 
 Player.prototype = {
-	update : function(){
-		if (this.keyboard.press(KEY_LEFT) && this.x > 14) {
-			this.dir = LEFT;
-			this.speedX = -1;
-			this.x += this.speedX ;
+  update: function () {
+    // if (this.keyboard.press(KEY_LEFT) && this.x > 14) {
+    //   console.log("KEY_LEFT");
+    //   this.dir = LEFT;
+    //   this.speedX = -1;
+    //   this.x += this.speedX;
+    // } else if (
+    //   this.keyboard.press(KEY_RIGHT) &&
+    //   this.x < this.context.canvas.width - 20
+    // ) {
+    //   console.log("KEY_RIGHT");
+    //   this.dir = RIGHT;
+    //   this.speedX = 1;
+    //   this.x += this.speedX;
+    // }
+    if (this.keyboard.press(KEY_LEFT)) {
+      this.dir = LEFT;
+      this.speedX = -1;
+      this.x += this.speedX;
+    }
+    if (this.keyboard.press(KEY_RIGHT)) {
+      this.dir = RIGHT;
+      this.speedX = 1;
+      this.x += this.speedX;
+    }
+  },
+  draw: function () {
+    this.context.drawImage(this.image, this.x, this.y);
+  },
+  power: function () {
+    this.fireball.push(
+      new Fireball(
+        this.context,
+        this.fireballImage,
+        this.x,
+        this.y + 5,
+        this.speedX * 3
+      )
+    );
+  },
+  input: function () {
+    this.keyboard.release(Z, function () {
+      console.log("POFDKPOASDK");
+    });
 
-		 }
-		 else if (this.keyboard.press(KEY_RIGHT) && 
-			this.x < this.context.canvas.width - 20) {
-			this.dir = RIGHT;
-			this.speedX = 1;
-			this.x += this.speedX;
-		 }
-	},
-	draw : function(){
-		this.context.drawImage(this.image, this.x , this.y);	
-
-	},	
-	power : function(){
-		this.fireball.push(new Fireball(this.context, this.fireballImage, this.x, this.y+5, this.speedX * 3));
-	},
-	
-
-
-}
+    if (this.keyboard.press(KEY_LEFT)) {
+      console.log("KEY_LEFT");
+    }
+    if (this.keyboard.press(KEY_RIGHT)) {
+      console.log("KEY_RIGHT");
+    }
+  },
+};
--- JavaScript/Utils/Keyboard.js
+++ JavaScript/Utils/Keyboard.js
@@ -2,41 +2,41 @@
 var KEY_LEFT = 37;
 var KEY_RIGHT = 39;
 var SPACE = 32;
-var Z =90;
+var Z = 90;
 var ENTER = 13;
 
-
 function Keyboard(element) {
-   this.element = element;
+  this.element = element;
 
-   this.pressed = [];
+  this.pressed = [];
 
-   this.released = [];
+  this.released = [];
 
-   this.releaseFunc = [];
+  this.releaseFunc = [];
 
-   var keyboard = this;
+  var keyboard = this;
 
-   element.addEventListener('keydown', function(event) {
-      var key = event.keyCode; 
-      keyboard.pressed[key] = true;
+  element.addEventListener("keydown", function (event) {
+    var key = event.keyCode;
+    keyboard.pressed[key] = true;
 
-      if (keyboard.releaseFunc[key] && !keyboard.released[key]) {
-          keyboard.released[key] = true;
-          keyboard.releaseFunc[key] () ;
-      }
-   });
+    if (keyboard.releaseFunc[key] && !keyboard.released[key]) {
+      keyboard.released[key] = true;
+      keyboard.releaseFunc[key]();
+    }
 
-   element.addEventListener('keyup', function(event) {
-      keyboard.pressed[event.keyCode] = false;
-      keyboard.released[event.keyCode] = false;
-   });
+  });
+
+  element.addEventListener("keyup", function (event) {
+    keyboard.pressed[event.keyCode] = false;
+    keyboard.released[event.keyCode] = false;
+  });
 }
 Keyboard.prototype = {
-   press: function(key) {
-      return this.pressed[key];
-   },
-   release: function(key, callback) {
-      this.releaseFunc[key] = callback;
-   }
-}
+  press: function (key) {
+    return this.pressed[key];
+  },
+  release: function (key, callback) {
+    this.releaseFunc[key] = callback;
+  },
+};
